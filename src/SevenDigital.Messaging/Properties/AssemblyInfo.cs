6de40a718f28beaf3d16682a4f01fa9eb0c0726b
Iain Ballard <iain.ballard@7digital.com>
2012-06-27 10:09:37 +0100

    packages

---
+++ src/SevenDigital.Messaging/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SevenDigital.Messaging")]
+[assembly: AssemblyDescription("A distributed contracts-based sender/handler messaging system built on RabbitMQ and MassTransit")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("7digital")]
+[assembly: AssemblyProduct("SevenDigital.Messaging")]
+[assembly: AssemblyCopyright("Copyright © 7digital 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45283910-64e2-4eb6-985a-7f6ff44c6c0c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
