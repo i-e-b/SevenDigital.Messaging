f72898f513ba6861d00b9d8c22cd237a56bd9ea3
Iain Ballard <iain.ballard@7digital.com>
2012-07-09 18:09:37 +0100

    Splitting imessage contract out

--- MergeMessagingAssemblies.cmd
+++ MergeMessagingAssemblies.cmd
@@ -1,3 +1,5 @@
 cd %~dp0\binaries
 
-..\tools\ILMerge.exe /allowDup /targetplatform:v4 /out:..\merged\SevenDigital.Messaging.dll SevenDigital.Messaging.dll Magnum.dll MassTransit.dll MassTransit.Transports.RabbitMq.dll RabbitMQ.Client.dll SevenDigital.Messaging.Types.dll StructureMap.dll 
+..\tools\ILMerge.exe /allowDup /targetplatform:v4 /out:..\merged\SevenDigital.Messaging.dll SevenDigital.Messaging.dll Magnum.dll MassTransit.dll MassTransit.Transports.RabbitMq.dll RabbitMQ.Client.dll StructureMap.dll 
+
+COPY SevenDigital.Messaging.Types.dll ..\merged\
