1512bc51f439ab457819903bfa5dfd19f7417db8
Iain Ballard <iain.ballard@7digital.com>
2012-09-05 14:39:16 +0100

    Added management to merge sources

--- BuildMergedAssemblies.cmd
+++ BuildMergedAssemblies.cmd
@@ -4,7 +4,9 @@ mkdir merged
 cd binaries
 
 copy "..\src\SevenDigital.Messaging.Base\bin\Release\*.dll" "." /Y
+copy "..\src\Messaging.Management\bin\Release\SevenDigital.Messaging.Management.dll" "."
 
 ..\tools\ILMerge.exe /allowDup /targetplatform:v4 /out:..\merged\SevenDigital.Messaging.dll SevenDigital.Messaging.Base.dll Magnum.dll MassTransit.dll MassTransit.Transports.RabbitMq.dll RabbitMQ.Client.dll StructureMap.dll 
 
 COPY SevenDigital.Messaging.Types.dll ..\merged\
+COPY SevenDigital.Messaging.Management.dll ..\merged\
