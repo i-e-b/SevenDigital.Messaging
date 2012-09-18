163b4db09675b562354f3b57815feaff7feb9f91
Iain Ballard <iain.ballard@7digital.com>
2012-09-18 16:07:49 +0100

    Changed unique listener endpoint to prevent release drift

--- src/SevenDigital.Messaging.Base/Routing/UniqueEndpointGenerator.cs
+++ src/SevenDigital.Messaging.Base/Routing/UniqueEndpointGenerator.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
-
 namespace SevenDigital.Messaging.Routing
 {
 	public class UniqueEndpointGenerator : IUniqueEndpointGenerator
@@ -11,16 +6,11 @@ namespace SevenDigital.Messaging.Routing
 
 		public UniqueEndpointGenerator()
 		{
-			var bytes = MD5.Create().ComputeHash(
-					Encoding.Default.GetBytes(Assembly.GetExecutingAssembly().CodeBase 
-					+ Naming.GetMacAddress())
-				);
 			strongName =  
 				Naming.MachineName()
 				+ "_" 
-				+ Naming.GoodAssemblyName() 
-				+ "_" 
-				+ Convert.ToBase64String(bytes).Replace("+", "").Replace("/", "").Replace("=", "");
+				+ Naming.GoodAssemblyName()
+				+ "_Listener";
 		
 		}
 
