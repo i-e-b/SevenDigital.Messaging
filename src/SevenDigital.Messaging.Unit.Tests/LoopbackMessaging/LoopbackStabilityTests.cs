3450a317271550fa31639b4d14e39e3cc88dc6d5
Iain Ballard <iain.ballard@7digital.com>
2012-11-16 16:31:13 +0000

    ClearEventHooks no longer removes loopback hook

---
+++ src/SevenDigital.Messaging.Unit.Tests/LoopbackMessaging/LoopbackStabilityTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SevenDigital.Messaging.EventHooks;
+using StructureMap;
+
+namespace SevenDigital.Messaging.Unit.Tests.LoopbackMessaging
+{
+	[TestFixture]
+	public class LoopbackStabilityTests
+	{
+		[Test]
+		public void clearing_event_hooks_after_settings_up_loopback_should_leave_loopback_hooks_in_place ()
+		{
+			new MessagingConfiguration().WithLoopback();
+
+			new MessagingConfiguration().ClearEventHooks();
+
+			Assert.That(
+				ObjectFactory.GetInstance<IEventHook>(),
+				Is.InstanceOf<TestEventHook>());
+		}
+	}
+}
