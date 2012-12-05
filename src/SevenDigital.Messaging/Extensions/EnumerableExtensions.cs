2896cf587d987e3f8e5bc84f38d5463576fb65cb
William Lord <william.lord@7digital.com>
2012-12-05 13:14:33 +0000

    Move EnumerableExtensions to System.Collections.Generic namespace

---
+++ src/SevenDigital.Messaging/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.Collections.Generic
+{
+	public static class EnumerableExtensions
+	{
+		public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+		{
+			foreach (var i in items) action(i);
+		}
+	}
+}
