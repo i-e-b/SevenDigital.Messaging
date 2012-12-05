ee465ae232ab9a91778e0aa3a64048ad623d2989
Iain Ballard <iain.ballard@7digital.com>
2012-12-05 17:42:49 +0000

    Polling in progress

--- src/SevenDigital.Messaging.Unit.Tests/Dispatch/DestinationMessagePollingTests.cs
+++ src/SevenDigital.Messaging.Unit.Tests/Dispatch/DestinationMessagePollingTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using System.Threading;
+using Moq;
 using NUnit.Framework;
 using SevenDigital.Messaging.Base;
 using SevenDigital.Messaging.Dispatch;
@@ -10,16 +11,22 @@ namespace SevenDigital.Messaging.Unit.Tests.Dispatch
 	{
 		IDestinationPoller subject;
 		Mock<IMessagingBase> messagingBase;
+		Mock<ISleepWrapper> sleeper;
+		Mock<IDispatcher> dispatcher;
+		Mock<IThreadPoolWrapper> pool;
 		string destinationName;
 
 		[SetUp]
 		public void A_destination_poller_with_a_destination_to_watch ()
 		{
 			messagingBase = new Mock<IMessagingBase>();
-			subject = new DestinationPoller(messagingBase.Object);
+			sleeper = new Mock<ISleepWrapper>();
+			dispatcher = new Mock<IDispatcher>();
+			pool = new Mock<IThreadPoolWrapper>();
 			destinationName = "a-destination";
-			subject.AddDestinationToWatch(destinationName);
-			subject.Start();
+			pool.Setup(m=>m.IsThreadAvailable()).Returns(true);
+
+			subject = new DestinationPoller(destinationName, messagingBase.Object, sleeper.Object, dispatcher.Object, pool.Object);
 		}
 
 		[TearDown]
@@ -31,23 +38,52 @@ namespace SevenDigital.Messaging.Unit.Tests.Dispatch
 		[Test]
 		public void Should_try_to_get_message_from_destination ()
 		{
+			subject.Start();
 			messagingBase.Verify(m=>m.GetMessage<IMessage>(destinationName));
-			subject.Stop();
 		}
 
 		[Test]
 		public void Should_sleep_if_no_message_found ()
 		{
+			messagingBase.Setup(m=>m.GetMessage<IMessage>(destinationName))
+				.Returns<IMessage>(null);
+			subject.Start();
+			Thread.Sleep(100);
+			sleeper.Verify(m=>m.Sleep());
 		}
 
 		[Test]
-		public void Should_try_to_get_another_message_if_message_found ()
+		public void Should_try_to_get_another_message_without_sleeping_if_message_found ()
 		{
+			messagingBase.Setup(m=>m.GetMessage<IMessage>(destinationName))
+				.Returns(new Mock<IMessage>().Object);
+			
+			subject.Start();
+			Thread.Sleep(100);
+			subject.Stop();
+			sleeper.Verify(m=>m.Sleep(), Times.Never());
 		}
 
 		[Test]
 		public void Should_dispatch_message_handlers_if_message_found ()
 		{
+			var fakeMsg = new Mock<IMessage>().Object;
+			messagingBase.Setup(m=>m.GetMessage<IMessage>(destinationName))
+				.Returns(fakeMsg);
+			
+			subject.Start();
+			Thread.Sleep(100);
+			subject.Stop();
+			dispatcher.Verify(m=>m.TryDispatch(fakeMsg));
+		}
+
+		[Test]
+		public void If_thread_pool_is_full_should_not_try_to_get_messages ()
+		{
+			pool.Setup(m=>m.IsThreadAvailable()).Returns(false);
+			subject.Start();
+			messagingBase.Verify(m=>m.GetMessage<IMessage>(destinationName), Times.Never());
+			sleeper.Verify(m=>m.Sleep());
 		}
 	}
 }
---
+++ src/SevenDigital.Messaging/Dispatch/SleepWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace SevenDigital.Messaging.Dispatch
+{
+	public class SleepWrapper : ISleepWrapper
+	{
+		public void Sleep()
+		{
+			Thread.Sleep(250);
+		}
+	}
+
+	public interface ISleepWrapper
+	{
+		void Sleep();
+	}
+}
