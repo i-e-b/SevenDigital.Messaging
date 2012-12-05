f2aaf029ff7f1a98771d681903d8ec783ddeb551
William Lord <william.lord@7digital.com>
2012-12-05 13:14:33 +0000

    Move EnumerableExtensions to System.Collections.Generic namespace

--- src/SevenDigital.Messaging/MessageSending/Loopback/LoopbackNodeFactory.cs
+++ src/SevenDigital.Messaging/MessageSending/Loopback/LoopbackNodeFactory.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using SevenDigital.Messaging.Routing;
 using StructureMap;
 
 namespace SevenDigital.Messaging.MessageSending.Loopback
