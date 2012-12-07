840371f55ae86695ccc9b5506fc13cc25f63eba0
Iain Ballard <iain.ballard@7digital.com>
2012-12-07 14:08:41 +0000

    Fixing loopback mode

---
+++ src/SevenDigital.Messaging.Integration.Tests/Loopback/LoopbackModeTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using SevenDigital.Messaging.EventHooks;
+using SevenDigital.Messaging.MessageSending;
+using StructureMap;
+
+namespace SevenDigital.Messaging.Integration.Tests.Loopback
+{
+	[TestFixture]
+	public class LoopbackModeTests
+	{
+		[Test]
+		public void Loopback_mode_configures_correctly ()
+		{
+			new MessagingConfiguration().WithLoopback();
+			
+			using (var listener = ObjectFactory.GetInstance<INodeFactory>().Listen())
+			{
+				listener.Handle<IMessage>().With<IntegrationHandler>();
+			}
+			
+			ObjectFactory.EjectAllInstancesOf<INodeFactory>();
+			ObjectFactory.EjectAllInstancesOf<INode>();
+			ObjectFactory.EjectAllInstancesOf<ITestEventHook>();
+		}
+	}
+
+	public class IntegrationHandler:IHandle<IMessage>
+	{
+		public void Handle(IMessage message)
+		{
+		}
+	}
+}
