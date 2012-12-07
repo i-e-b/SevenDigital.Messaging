ed905e9ad7731c3342f5fe373539be500ed23b7e
Iain Ballard <iain.ballard@7digital.com>
2012-12-07 09:36:04 +0000

    First try at new merge set

--- BuildMergedAssemblies.cmd
+++ BuildMergedAssemblies.cmd
@@ -3,10 +3,8 @@ mkdir binaries
 mkdir merged
 cd binaries
 
-copy "..\src\SevenDigital.Messaging.Base\bin\Release\*.dll" "." /Y
-copy "..\src\Messaging.Management\bin\Release\SevenDigital.Messaging.Management.dll" "."
+copy "..\src\SevenDigital.Messaging\bin\Release\*.dll" "." /Y
 
 ..\tools\ILMerge.exe /allowDup /targetplatform:"v4,c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\merged\SevenDigital.Messaging.dll SevenDigital.Messaging.dll RabbitMQ.Client.dll StructureMap.dll SevenDigital.Messaging.Base.dll SignalHandling.dll
 
 COPY SevenDigital.Messaging.Types.dll ..\merged\
-COPY SevenDigital.Messaging.Management.dll ..\merged\
