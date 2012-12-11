139951d1afef6562af5950ecf8fa2b56550f0792
Iain Ballard <iain.ballard@7digital.com>
2012-12-11 12:51:41 +0000

    Message query config

---
+++ src/SevenDigital.Messaging.StructureMap.Unit.Tests/ConfigureWithMessageQueryTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using SevenDigital.Messaging.Base.RabbitMq.RabbitMqManagement;
+using StructureMap;
+
+namespace SevenDigital.Messaging.StructureMap.Unit.Tests
+{
+	[TestFixture]
+	public class ConfigureWithMessageQueryTests
+	{
+		IRabbitMqQuery subject;
+
+		[SetUp]
+		public void Configured_with_message_query ()
+		{
+			new MessagingConfiguration().WithMessagingQuery("host", "user", "pass", "vhost");
+			subject = ObjectFactory.GetInstance<IRabbitMqQuery>();
+		}
+
+		[Test]
+		public void Should_get_query_with_configured_host ()
+		{
+			Assert.That(subject.HostUri.ToString(), Is.EqualTo("http://host:55672/"));
+		}
+
+		[Test]
+		public void Should_get_host_with_configured_vhost ()
+		{
+			Assert.That(subject.VirtualHost, Is.EqualTo("/vhost"));
+		}
+
+		[Test]
+		public void Should_get_host_with_configured_credentials ()
+		{
+			Assert.That(subject.Credentials.Password, Is.EqualTo("pass"));
+			Assert.That(subject.Credentials.UserName, Is.EqualTo("user"));
+		}
+	}
+}
