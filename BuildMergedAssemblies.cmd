769401cb838a512548b64cc3fa90d56768fcb358
Iain Ballard <iain.ballard@7digital.com>
2013-02-04 12:18:42 +0000

    New merged, with shift-it lib

--- BuildMergedAssemblies.cmd
+++ BuildMergedAssemblies.cmd
@@ -5,6 +5,6 @@ cd binaries
 
 copy "..\src\SevenDigital.Messaging\bin\Release\*.dll" "." /Y
 
-..\tools\ILMerge.exe /allowDup /targetplatform:"v4,c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\merged\SevenDigital.Messaging.dll SevenDigital.Messaging.dll RabbitMQ.Client.dll StructureMap.dll SevenDigital.Messaging.Base.dll SignalHandling.dll
+..\tools\ILMerge.exe /allowDup /targetplatform:"v4,c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\merged\SevenDigital.Messaging.dll SevenDigital.Messaging.dll RabbitMQ.Client.dll StructureMap.dll SevenDigital.Messaging.Base.dll SignalHandling.dll ShiftIt.dll
 
 COPY SevenDigital.Messaging.Types.dll ..\merged\
