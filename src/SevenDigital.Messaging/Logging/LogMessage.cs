2e2814fecc81426f3583353bd6d8776ab3edec00
Iain Ballard <iain.ballard@7digital.com>
2013-02-06 17:30:12 +0000

    Time in log

--- src/SevenDigital.Messaging/Logging/LogMessage.cs
+++ src/SevenDigital.Messaging/Logging/LogMessage.cs
@@ -1,12 +1,20 @@
+using System;
+
 namespace SevenDigital.Messaging.Logging
 {
 	public class LogMessage : ILogMessage
 	{
+		public LogMessage()
+		{
+			LogDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+		}
 		public string Message { get; set; }
+		public string LogDate { get; set; }
 	}
 
 	public interface ILogMessage
 	{
 		string Message { get; set; }
+		string LogDate { get; set; }
 	}
 }
