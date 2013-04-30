f64a276de4a1c257e31a89acba1990bccc47a904
William Lord <william.lord@7digital.com>
2013-04-30 14:26:29 +0100

    Update build.cmd to use platform agnostic rakefile

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd %~dp0..
 
-rake -f Build\build-win.rb "build:no_test_build[., SevenDigital.Messaging.sln, build, full, local, Release]" 
+rake -f build\build.rb "build:no_test_build[., SevenDigital.Messaging.sln, build, full, local, Release]" 
