d9f2c5fde817660f96cf88fbab19e02df0a69b89
Iain Ballard <iain.ballard@7digital.com>
2013-05-28 15:35:07 +0100

    Removing redundant sender endpoint generator

--- src/SevenDigital.Messaging/Routing/IEndpointGenerator.cs
+++ src/SevenDigital.Messaging/Routing/IEndpointGenerator.cs
@@ -1,10 +1,18 @@
 namespace SevenDigital.Messaging.Routing
 {
+	/// <summary>
+	/// General contract for generating the name of a messaging endpoint
+	/// </summary>
 	public interface IEndpointGenerator
 	{
+		/// <summary>
+		/// Create an endpoint name
+		/// </summary>
 		Endpoint Generate();
 	}
 
+	/// <summary>
+	/// Auto generator that produces unique names every time it is called
+	/// </summary>
 	public interface IUniqueEndpointGenerator : IEndpointGenerator {}
-	public interface ISenderEndpointGenerator : IEndpointGenerator {}
 }
