2128359d20f1774518d8b7c95b3c1e0821b6388d
Iain Ballard <iain.ballard@7digital.com>
2013-05-31 15:42:54 +0100

    Old configuration is gone

--- src/SevenDigital.Messaging.Integration.Tests/Hooks/EventHookTests.cs
+++ src/SevenDigital.Messaging.Integration.Tests/Hooks/EventHookTests.cs
@@ -4,7 +4,6 @@ using Moq;
 using NUnit.Framework;
 using SevenDigital.Messaging.Integration.Tests.Handlers;
 using SevenDigital.Messaging.Integration.Tests.Messages;
-using SevenDigital.Messaging.MessageReceiving;
 using StructureMap;
 
 namespace SevenDigital.Messaging.Integration.Tests
