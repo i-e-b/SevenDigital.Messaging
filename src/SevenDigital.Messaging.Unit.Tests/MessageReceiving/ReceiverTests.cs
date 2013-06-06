a8de152b9d828aa7a03d22fc119171609d6dde14
Iain Ballard <iain.ballard@7digital.com>
2013-06-06 17:57:06 +0100

    Stubs for remaining tests

---
+++ src/SevenDigital.Messaging.Unit.Tests/MessageReceiving/ReceiverTests.cs
@@ -0,0 +1,43 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using SevenDigital.Messaging.Base.Routing;
+using SevenDigital.Messaging.Infrastructure;
+using SevenDigital.Messaging.MessageReceiving;
+using SevenDigital.Messaging.MessageReceiving.RabbitPolling;
+using SevenDigital.Messaging.Routing;
+
+namespace SevenDigital.Messaging.Unit.Tests.MessageReceiving
+{
+	[TestFixture]
+	public class ReceiverTests
+	{
+		Receiver _subject;
+		IUniqueEndpointGenerator _endpointGenerator;
+		IHandlerManager _handlerManager;
+		IMessageRouter _messageRouter;
+		IPollingNodeFactory _pollerFactory;
+		IDispatcherFactory _dispatchFactory;
+
+		[SetUp]
+		public void setup()
+		{
+			_endpointGenerator = Substitute.For<IUniqueEndpointGenerator>();
+			_handlerManager = Substitute.For<IHandlerManager>();
+			_messageRouter = Substitute.For<IMessageRouter>();
+			_pollerFactory = Substitute.For<IPollingNodeFactory>();
+			_dispatchFactory = Substitute.For<IDispatcherFactory>();
+
+			_subject = new Receiver(
+				_endpointGenerator,
+				_handlerManager,
+				_messageRouter, _pollerFactory, _dispatchFactory);
+		}
+
+		[Test]
+		public void _todo()
+		{
+			_subject.Dispose();
+			Assert.Inconclusive();
+		}
+	}
+}
---
+++ src/SevenDigital.Messaging.Unit.Tests/MessageSending/SenderNodeTests.cs
@@ -0,0 +1,33 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using SevenDigital.Messaging.Base;
+using SevenDigital.Messaging.Infrastructure;
+using SevenDigital.Messaging.MessageSending;
+
+namespace SevenDigital.Messaging.Unit.Tests.MessageSending
+{
+	[TestFixture]
+	public class SenderNodeTests
+	{
+		ISenderNode _subject;
+		IMessagingBase _messagingBase;
+		IDispatcherFactory _dispatcherFactory;
+
+		[SetUp]
+		public void setup()
+		{
+			_messagingBase = Substitute.For<IMessagingBase>();
+			_dispatcherFactory = Substitute.For<IDispatcherFactory>();
+
+			_subject = new SenderNode(_messagingBase, _dispatcherFactory);
+		}
+
+		[Test]
+		public void _todo()
+		{
+			_subject.Dispose();
+			Assert.Inconclusive();
+		}
+
+	}
+}
