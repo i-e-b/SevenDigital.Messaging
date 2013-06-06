1fc74387b51bc1023f455734897994697d9d1866
Iain Ballard <iain.ballard@7digital.com>
2013-06-06 17:57:06 +0100

    Stubs for remaining tests

--- src/SevenDigital.Messaging/MessageSending/HandlerAction.cs
+++ src/SevenDigital.Messaging/MessageSending/HandlerAction.cs
@@ -5,5 +5,5 @@ namespace SevenDigital.Messaging.MessageSending
 	/// <summary>
 	/// Generic action performed by a handler. This maps to `IHandle&lt;T&gt;`
 	/// </summary>
-	public delegate Exception HandlerAction<T>(T message) where T : class, IMessage;
+	public delegate Exception HandlerAction<in T>(T message) where T : class, IMessage;
 }
