2456e8e9a6cc88d3a5cfdefd16973227f97ed177
Iain Ballard <iain.ballard@7digital.com>
2013-06-11 11:44:55 +0100

    Nuget package

--- src/SevenDigital.Messaging/Properties/AssemblyInfo.cs
+++ src/SevenDigital.Messaging/Properties/AssemblyInfo.cs
@@ -2,15 +2,14 @@
 using System.Resources;
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.2.0")]
+
 [assembly: AssemblyTitle("SevenDigital.Messaging")]
 [assembly: AssemblyDescription("A distributed contracts-based sender/handler messaging system")]
 
 [assembly: AssemblyProduct("SevenDigital.Messaging")]
 
-
-
 [assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: InternalsVisibleTo("SevenDigital.Messaging.Unit.Tests")]
 [assembly: InternalsVisibleTo("SevenDigital.Messaging.Integration.Tests")]
-[assembly: AssemblyFileVersion("1")]
-[assembly: AssemblyVersion("1")]
