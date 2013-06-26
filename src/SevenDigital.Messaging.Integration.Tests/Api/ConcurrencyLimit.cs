a3189526d66a50f2750b6d2b37b707bb90979b57
Iain Ballard <iain.ballard@7digital.com>
2013-06-26 14:39:07 +0100

    Fixing loopback concurrency bug

---
+++ src/SevenDigital.Messaging.Integration.Tests/Api/ConcurrencyLimit.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace SevenDigital.Messaging.Integration.Tests
+{
+	[TestFixture]
+	public class ConcurrencyLimit
+	{
+		[Test]
+		public void can_set_concurreny_limit_in_default_mode ()
+		{
+			MessagingSystem.Configure.WithDefaults();
+			MessagingSystem.Control.SetConcurrentHandlers(1);
+			MessagingSystem.Control.Shutdown();
+
+			Assert.Pass();
+		}
+		
+		[Test]
+		public void can_set_concurreny_limit_in_loopback_mode ()
+		{
+			MessagingSystem.Configure.WithLoopbackMode();
+			MessagingSystem.Control.SetConcurrentHandlers(1);
+			MessagingSystem.Control.Shutdown();
+
+			Assert.Pass();
+		}
+	}
+}
