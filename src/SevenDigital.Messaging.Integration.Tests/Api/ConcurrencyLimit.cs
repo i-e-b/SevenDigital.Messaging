e7db5c22a962ddd22dd8c8b0e4baeb9dfb55eb66
Iain Ballard <iain.ballard@7digital.com>
2013-07-03 16:27:02 +0100

    Concurrency test

--- src/SevenDigital.Messaging.Integration.Tests/Api/ConcurrencyLimit.cs
+++ src/SevenDigital.Messaging.Integration.Tests/Api/ConcurrencyLimit.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Threading;
+using NUnit.Framework;
+using SevenDigital.Messaging.Integration.Tests.Messages;
 
 namespace SevenDigital.Messaging.Integration.Tests
 {
@@ -14,7 +16,41 @@ namespace SevenDigital.Messaging.Integration.Tests
 
 			Assert.Pass();
 		}
-		
+
+		[Test]
+		public void concurrency_limit_is_obeyed_in_default_mode ()
+		{
+			MessagingSystem.Configure.WithDefaults().SetIntegrationTestMode();
+			MessagingSystem.Control.SetConcurrentHandlers(1);
+
+			CountingHandler.MaxCount = 0;
+			CountingHandler.Count = 0;
+			MessagingSystem.Receiver().Listen().Register(
+				with => with.Handle<IMessage>().With<CountingHandler>());
+
+			for (int i = 0; i < 20; i++)
+			{
+				MessagingSystem.Sender().SendMessage(new GreenMessage());
+			}
+
+			Thread.Sleep(1500);
+			MessagingSystem.Control.Shutdown();
+			Assert.That(CountingHandler.MaxCount, Is.EqualTo(1));
+		}
+
+		public class CountingHandler:IHandle<IMessage>
+		{
+			public static volatile int Count, MaxCount;
+
+			public void Handle(IMessage message)
+			{
+				Interlocked.Increment(ref Count);
+				Thread.Sleep(200);
+				if (Count > MaxCount) MaxCount = Count;
+				Interlocked.Decrement(ref Count);
+			}
+		}
+
 		[Test]
 		public void can_set_concurreny_limit_in_loopback_mode ()
 		{
